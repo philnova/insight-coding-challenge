d88390ac9f4a84a820b853e52aa1e627a2a25f64
Phil Nova <pnova8@gmail.com>
2017-04-04 00:29:21 -0700

    rewrite input data reader as object

--- insight_testsuite/temp/src/read_input_data.py
+++ insight_testsuite/temp/src/read_input_data.py
@@ -1,11 +1,14 @@
 import csv
 import read_server_log
 
-def read(input_file):
-    with open(input_file, 'rb') as f_input:
-        csv_input = csv.reader(f_input, delimiter=' ')
-        data = []
-        for row in csv_input:
-            log_line = read_server_log.ServerLog(row)
-            data.append(log_line)
-    return data
+class FileInputReader(object):
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.data = []
+
+    def read(self):
+        with open(self.input_file, 'rb') as f_input:
+            csv_input = csv.reader(f_input, delimiter=' ')
+            for row in csv_input:
+                log_line = read_server_log.ServerLog(row)
+                self.data.append(log_line)
